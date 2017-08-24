3e523140413c0a7b0b2af37f0314b48e56d8faf9
Sahil <sahilhaque24.10@gmail.com>
2017-08-24 22:20:59 +0530

    styled large-hero

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,13 +2,14 @@ var gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssImport = require('postcss-import'),
-cssvars = require('postcss-simple-vars'),
 mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba'),
+cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested');
 
 gulp.task('styles', function(){
    return gulp.src('./app/assets/styles/style.css')
-            .pipe(postcss([cssImport, mixins, autoprefixer, cssvars, nested]))
+            .pipe(postcss([cssImport, hexrgba, mixins, autoprefixer, cssvars, nested]))
             .on('error', function(errorInfo){
                 console.log(errorInfo.toString());
                 this.emit('end');
