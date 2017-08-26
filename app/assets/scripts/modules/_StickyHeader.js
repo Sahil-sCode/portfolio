67496bc0c5ee919c7974632593a1cbc61c9b41a4
Sahil <sahilhaque24.10@gmail.com>
2017-08-26 22:38:36 +0530

    applied flex and done few adjustment

---
+++ app/assets/scripts/modules/_StickyHeader.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class StickyHeader {
+    constructor(){
+        
+    }
+}
+
+export default StickyHeader;
